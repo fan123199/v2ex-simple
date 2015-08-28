36d21b3215df1def3c3256adfa765cb90742a86d
fan123199 <fan123199@gmail.com>
2015-08-28 18:11:41 +0800

    Add a clicklistener for recyclerview

---
+++ app/src/main/java/im/fdx/v2ex/utils/ClickListener.java
@@ -0,0 +1,13 @@
+package im.fdx.v2ex.utils;
+
+import android.view.View;
+
+/**
+ * Created by a708 on 15-8-28.
+ */
+public interface ClickListener {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+
+}
---
+++ app/src/main/java/im/fdx/v2ex/utils/RecyclerTouchListener.java
@@ -0,0 +1,58 @@
+package im.fdx.v2ex.utils;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by a708 on 15-8-28.
+ */
+public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+    private GestureDetector mGestureDetector;
+    private ClickListener mListener;
+
+    public RecyclerTouchListener(Context context, final RecyclerView recyclerView, ClickListener clickListener) {
+
+        mListener = clickListener;
+
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                L.m("single tap up");
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+                View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if (childView != null && mListener != null) {
+                    mListener.onLongClick(childView, recyclerView.getChildAdapterPosition(childView));
+                    L.m("on Long Press");
+                }
+            }
+        });
+    }
+
+    //onInterceptTouchEvent默认值是false这样才能把事件传给View里的onTouchEvent.
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        View childView = rv.findChildViewUnder(e.getX(), e.getY());
+
+        if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onClick(childView, rv.getChildAdapterPosition(childView));
+            L.m("why do onIntercept");
+        }
+
+        return false;
+    }
+    //默认返回true
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+}
