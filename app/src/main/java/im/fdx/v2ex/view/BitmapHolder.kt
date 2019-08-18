1b9fa4a993c8c9673d8a6b8d122af026d2bd62ab
fdx <fan123199@gmail.com>
2019-08-18 16:39:56 +0800

    remove weakreference because image will disappear.

--- app/src/main/java/im/fdx/v2ex/view/BitmapHolder.kt
+++ app/src/main/java/im/fdx/v2ex/view/BitmapHolder.kt
@@ -5,12 +5,11 @@ import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import im.fdx.v2ex.MyApp
 import im.fdx.v2ex.R
-import java.lang.ref.WeakReference
 
 @Suppress("DEPRECATION")
 class BitmapHolder : BitmapDrawable() {
 
-    private var vDrawable: WeakReference<Drawable>? = null
+    private var vDrawable: Drawable? = null
 
     override fun draw(canvas: Canvas) {
         if(vDrawable == null) {
@@ -18,11 +17,11 @@ class BitmapHolder : BitmapDrawable() {
 //            val d = MyApp.get().getDrawable(R.drawable.ic_github)
 //            d?.draw(canvas)
         } else {
-            vDrawable?.get()?.draw(canvas)
+            vDrawable?.draw(canvas)
         }
     }
 
     fun setDrawable(drawable: Drawable) {
-        this.vDrawable = WeakReference(drawable)
+        this.vDrawable = drawable
     }
 }
