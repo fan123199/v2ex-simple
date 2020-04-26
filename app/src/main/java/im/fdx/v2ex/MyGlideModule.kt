f11e5dd723283a3a4cefedd78a29356fd01bfebb
fdx <fan123199@gmail.com>
2020-04-26 21:18:12 +0800

    Use GlideApp instead of Glide

---
+++ app/src/main/java/im/fdx/v2ex/MyGlideModule.kt
@@ -0,0 +1,11 @@
+package im.fdx.v2ex
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by fdx on 2017/8/29.
+ *
+ */
+@GlideModule
+class MyGlideModule : AppGlideModule()
