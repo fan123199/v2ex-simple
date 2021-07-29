d41b9a3408516b53159020a2a019c3f34da9baeb
fdx <fan123199@gmail.com>
2021-07-30 01:18:53 +0800

    update gradle to 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         maven("https://maven.fabric.io/public")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath("com.google.gms:google-services:4.3.8")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
