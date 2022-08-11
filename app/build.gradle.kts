4b897848872c4f0e9e05f09b672d7681f35a15bf
fdx <fan123199@gmail.com>
2022-08-11 12:02:18 +0800

    update app version to 2.7.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "im.fdx.v2ex"
         minSdk = 22
         targetSdk = 31
-        versionCode = 48
-        versionName = "2.7.4"
+        versionCode = 49
+        versionName = "2.7.5"
     }
     lint {
         isCheckReleaseBuilds = false
