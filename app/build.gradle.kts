dd4389f420bbbeaec40c7ff388f491df85773eb6
fdx <fan123199@gmail.com>
2022-11-25 14:19:38 +0800

    2.8.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "im.fdx.v2ex"
         minSdk = 22
         targetSdk = 33
-        versionCode = 57
-        versionName = "2.8.6"
+        versionCode = 58
+        versionName = "2.8.7"
     }
     lint {
         checkReleaseBuilds = false
